8f8e18ab83aa521996e670acd53f4c27f7e0943d
Julio Rocha <juliorochafoz@gmail.com>
2022-03-10 14:02:03 -0300

    [Setup]: Config ts and start server

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  //   if (err) {
+  //     return console.error(err);
+  //   }
+  return console.log(`[API] server is running on ${port}`);
+});
